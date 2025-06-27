defee96b184aab8ec9fd33d814a277aee27f33b1
isacraraujo <1s4c.3d@gmail.com>
2025-06-27 08:37:24 -0300

    feature(application): suíte e caso de teste de login com relatório #done

--- cypress/e2e/application/01.efetuar-login.cy.js
+++ cypress/e2e/application/01.efetuar-login.cy.js
@@ -1,7 +1,20 @@
 describe('Suíte de Teste: Efetuar Login', () => {
+  const url_application = Cypress.env('url_application')
+  const userlogin       = Cypress.env('user_auth')
+  const passwordlogin   = Cypress.env('password_auth')
 
-  it('Description', () => {
-    cy.visit('https://example.cypress.io')
+  before(() => {
+    // Ajustar a resolução para o 1368x768 conforme o cypress.config.js
+    cy.viewport(Cypress.config('desktopViewWidth'), Cypress.config('desktopViewHeight'))
+  });
+
+  it('Deve Fazer Login com Sucesso', () => {
+    cy.visit(`${url_application}/login`)
+    cy.contains('h1','Login').should('be.visible')
+    cy.get('[data-testid="email"]').type(userlogin)
+    cy.get('[data-testid="senha"]').type(passwordlogin)
+    cy.get('[data-testid="entrar"]').click()
+    cy.contains('h1','Bem Vindo').should('be.visible')
   });
 
 });

d44834ca73caa3c2c9f9958c5d6aea006f7ac5e5
isacraraujo <1s4c.3d@gmail.com>
2025-06-27 08:38:14 -0300

    update: variáveis de ambiente e configurações de responsividade para execução em desktop

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,21 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env: {
-    url_application: 'https://front.serverest.dev/',
-    url_api:         'https://serverest.dev/',
+    url_application: 'https://front.serverest.dev',
+    url_api:         'https://serverest.dev',
+    user_auth:       'isac@qa.com',
+    password_auth:   '123456'
   },
   e2e: {
     setupNodeEvents(on, config) {
       // exportar o módulo de relatórios do Mochawesome
       require('cypress-mochawesome-reporter/plugin')(on);
+      // configurações de responsividade mínima para desktop
+      config.desktopViewWidth  = 1368;
+      config.desktopViewHeight = 768;
+      return config;
     },
+    
   },
   // configurações personalizadas do relatório Mochawesome
   reporter: 'cypress-mochawesome-reporter',
